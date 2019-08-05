efcd639830bd3af5f5c4ff3891886c4b4f9aeeb5
Diego Moreira <diegomptcc@hotmail.com>
2019-08-05 20:38:04 -0300

    configure prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,9 @@ module.exports = {
     node: true,
   },
   extends: [
-    'airbnb-base',
+    'airbnb-base', 'prettier'
   ],
+  plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -15,6 +16,7 @@ module.exports = {
     ecmaVersion: 2018,
   },
   rules: {
+    "prettier/prettier": "error",
     "class-methods-use-this": "off",
     "no-param-reassign": "off",
     "camelcase": "off",
