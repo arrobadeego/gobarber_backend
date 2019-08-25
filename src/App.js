916ffdbeaf8794744eb72e5de3698b043d38fc83
Diego Moreira <diegomptcc@hotmail.com>
2019-08-24 21:54:03 -0300

    fix export modules

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const routes = require('./routes');
 
 require('./database');
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
 const { Router } = require('express');
 
+const User = require('./app/models/User');
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello world' }));
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Diego Moreira',
+        email: 'diego@teste.com',
+        password_hash: '123456',
+    });
+
+    return res.json(user);
+});
 
 module.exports = routes;
