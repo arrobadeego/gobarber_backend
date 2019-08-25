6a7371f3e99f51d92614cab6adc4557d44759562
Diego Moreira <diegomptcc@hotmail.com>
2019-08-25 19:51:07 -0300

    import File model on database index

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,11 @@
 const Sequelize = require('sequelize');
 
 const User = require('../app/models/User');
+const File = require('../app/models/File');
 
 const databaseConfig = require('../config/database');
 
-const models = [User];
+const models = [User, File];
 
 class Database {
     constructor() {
