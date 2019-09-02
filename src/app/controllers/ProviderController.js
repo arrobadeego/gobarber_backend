79c031f2b39344f823fa7da9270fb6f4629f70ef
Diego Moreira <diegomptcc@hotmail.com>
2019-09-02 10:40:09 -0300

    create Provider controller and index method with route

---
+++ src/app/controllers/ProviderController.js
@@ -0,0 +1,11 @@
+const User = require('../models/User');
+
+class ProviderController {
+    async index(req, res) {
+        const providers = await User.findAll({ where: { provider: true } });
+
+        return res.json(providers);
+    }
+}
+
+module.exports = new ProviderController();
