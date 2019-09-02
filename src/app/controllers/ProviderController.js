63d4d8401540d03ee38b047fe94a8de0cd82eab2
Diego Moreira <diegomptcc@hotmail.com>
2019-09-02 10:51:53 -0300

    change return on index Provider controller

--- src/app/controllers/ProviderController.js
+++ src/app/controllers/ProviderController.js
@@ -1,8 +1,13 @@
 const User = require('../models/User');
+const File = require('../models/File');
 
 class ProviderController {
     async index(req, res) {
-        const providers = await User.findAll({ where: { provider: true } });
+        const providers = await User.findAll({
+            where: { provider: true },
+            attributes: ['id', 'name', 'email', 'avatar_id'],
+            include: [File],
+        });
 
         return res.json(providers);
     }
