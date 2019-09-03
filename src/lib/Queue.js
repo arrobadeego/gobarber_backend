3d6f531b8d251340262cd6b79cdb9c91b983dccc
Diego Moreira <diegomptcc@hotmail.com>
2019-09-03 14:49:45 -0300

    create Queue file

---
+++ src/lib/Queue.js
@@ -0,0 +1,13 @@
+const Bee = require('bee-queue');
+
+class Queue {
+    constructor() {
+        this.queues = {};
+
+        this.init();
+    }
+
+    init() {}
+}
+
+module.exports = new Queue();
