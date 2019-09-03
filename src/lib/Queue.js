7ce8d542e5819f6ed94e74a2dfa39465812f4cff
Diego Moreira <diegomptcc@hotmail.com>
2019-09-03 15:39:20 -0300

    create add and process of bees on Redis

--- src/lib/Queue.js
+++ src/lib/Queue.js
@@ -1,6 +1,7 @@
 const Bee = require('bee-queue');
 
 const CancellationMail = require('../jobs/CancellationMail');
+const redisConfig = require('../config/redis');
 
 const jobs = [CancellationMail];
 
@@ -13,11 +14,24 @@ class Queue {
 
     init() {
         jobs.forEach(({ key, handle }) => {
-            this.queues[key] = new Bee(key, {
-                redis: {
-                    
-                }
-            })
+            this.queues[key] = {
+                bee: new Bee(key, {
+                    redis: redisConfig,
+                }),
+                handle,
+            };
+        });
+    }
+
+    add(queue, job) {
+        return this.queues[queue].bee.createJob(job).save();
+    }
+
+    processQueue() {
+        jobs.forEach(job => {
+            const { bee, handle } = this.queues[job.key];
+
+            bee.process(handle);
         });
     }
 }
