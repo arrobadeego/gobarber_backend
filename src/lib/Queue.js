ab281bfdcd2dbd9b98ff69cce595cc716897919b
Diego Moreira <diegomptcc@hotmail.com>
2019-09-03 15:04:58 -0300

    initalize Queue init

--- src/lib/Queue.js
+++ src/lib/Queue.js
@@ -1,5 +1,9 @@
 const Bee = require('bee-queue');
 
+const CancellationMail = require('../jobs/CancellationMail');
+
+const jobs = [CancellationMail];
+
 class Queue {
     constructor() {
         this.queues = {};
@@ -7,7 +11,15 @@ class Queue {
         this.init();
     }
 
-    init() {}
+    init() {
+        jobs.forEach(({ key, handle }) => {
+            this.queues[key] = new Bee(key, {
+                redis: {
+                    
+                }
+            })
+        });
+    }
 }
 
 module.exports = new Queue();
