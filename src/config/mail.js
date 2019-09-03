b3ee93946cbaeabb7abb3e175cfa24d57cdcfbc0
Diego Moreira <diegomptcc@hotmail.com>
2019-09-03 08:55:08 -0300

    configure e-mail

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+module.exports = {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: 'edd462f04036fe',
+        pass: 'efbc948337507c',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>',
+    },
+};
