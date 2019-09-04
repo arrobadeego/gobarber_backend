a5a4632cc4a0f03d8ca41a8aa86f3f0ce4049d59
Diego Moreira <diegomptcc@hotmail.com>
2019-09-04 09:26:02 -0300

    configure Sentry on App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const Sentry = require('@sentry/node');
+
 const routes = require('./routes');
+const sentryConfig = require('./config/sentry');
 
 require('./database');
 
@@ -9,11 +11,14 @@ class App {
     constructor() {
         this.server = express();
 
+        Sentry.init(sentryConfig);
+
         this.middlewares();
         this.routes();
     }
 
     middlewares() {
+        this.server.use(Sentry.Handlers.requestHandler());
         this.server.use(express.json());
         this.server.use(
             '/files',
@@ -23,6 +28,7 @@ class App {
 
     routes() {
         this.server.use(routes);
+        this.server.use(Sentry.Handlers.errorHandler());
     }
 }
 
