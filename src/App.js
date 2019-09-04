bddc5265e58296c9b08b53bc577a3a2a749f62e4
Diego Moreira <diegomptcc@hotmail.com>
2019-09-04 09:51:18 -0300

    implement on app exceptionhandler

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const Sentry = require('@sentry/node');
-
+const Youch = require('youch');
 require('express-async-errors');
 
 const routes = require('./routes');
@@ -17,6 +17,7 @@ class App {
 
         this.middlewares();
         this.routes();
+        this.exceptionHandler();
     }
 
     middlewares() {
@@ -31,6 +32,14 @@ class App {
     routes() {
         this.server.use(routes);
         this.server.use(Sentry.Handlers.errorHandler());
+    }
+
+    exceptionHandler() {
+        this.server.use(async (err, req, res, next) => {
+            const errors = await new Youch(err, req).toJSON();
+
+            return res.status(500).json(errors);
+        });
     }
 }
 
